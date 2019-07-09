f1f22c402a8d5968cee82db618b2cac738347ff2
5p4r70n <jothishwp8@outlook.com>
2019-07-09 19:50:07 +0530

    finally worked submitting

--- Frontend/src/app/fir/fir.component.ts
+++ Frontend/src/app/fir/fir.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient
+} from "@angular/common/http"
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-fir',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirComponent implements OnInit {
 
-  constructor() { }
+  Prathy = '';
+  Vaadi = '';
+  Tittle = '';
+  CType = '';
+  WriteUp = '';
+  image: File;
+  msgData:Object={data:false};
+
+
+  constructor(
+    private http:HttpClient
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log ('Haiii');
+    let newFir ={Prathy:this.Prathy,Vaadi:this.Vaadi,Tittle:this.Tittle,WriteUp:this.WriteUp,image:this.image}
+    console.log(newFir);
   }
 
 }
