3aae0a931e9e50907b08cbb3b70df22d571db0ed
5p4r70n <jothishwp8@outlook.com>
2019-08-03 12:10:07 +0530

    home both logins are works

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -4,10 +4,10 @@ import {
 } from '@angular/core';
 import {
   HttpClient
-} from '@angular/common/http'
+} from '@angular/common/http';
 import {
   Router
-} from "@angular/router"
+} from '@angular/router';
 import {
   from
 } from 'rxjs';
@@ -22,9 +22,9 @@ export class HomeComponent implements OnInit {
   Password: string; // received from frontend
   MasterPass: string; // received from server
 
-  Station:any; //respose received from server
+  Station: any; // respose received from server
   StationAddr: any; // received from frntend
-  StPass: any; //received from frnt end
+  StPass: any; // received from frnt end
 
 
 
@@ -34,32 +34,40 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() { // this will load the station lists from blockchain on startup
     const url = ' http://127.0.0.1:3000/getPoSt';
-    this.http.get(url).subscribe((res)=>{
+    this.http.get(url).subscribe((res) => {
       // console.log(res + "Police station response");
-      this.Station= res;
-      //console.log(res[1].name + "response" );
-    })
+      this.Station = res;
+      // console.log(res[1].name + "response" );
+    });
 
   }
 
   login() {
-    const url= 'http://127.0.0.1:3000/getMpass';
-    this.http.get(url).subscribe((res : string) => {
+    const url = 'http://127.0.0.1:3000/getMpass';
+    this.http.get(url).subscribe((res: string) => {
       console.log(res[0]);
       this.MasterPass = res[0];
-    })
-    console.log(this.Password)
-    if (this.Password === this.MasterPass)
-    {
-      this.router.navigate(['/MasterPage'])
+    });
+    console.log(this.Password);
+    if (this.Password === this.MasterPass) {
+      this.router.navigate(['/MasterPage']);
     } else {
       console.log('Password Missmatch');
       }
 
   }
 
-  StLog() {
-
-
+  stLog() {
+    // console.log(this.StationAddr + '  ' + this.StPass + ' received from server'  );
+    for (const Stat of this.Station) {  //this for loop will load station list got from server and rotate
+      // console.log (Stat[0]  + 'inside loop');
+      if (this.StationAddr === Stat.StatAddr) { //this if else will check frnd address find the address including array
+        if (this.StPass === Stat.Password) { //this check wether the given password is right or not
+          this.router.navigate(['/fir']); // finelly it will redirect to fir registration form
+        } else {
+          console.log('passwordMissMatch');
+        }
+      }
+    }
   }
 }
